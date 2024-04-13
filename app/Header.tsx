aa4fdc3d43b7c7f438519ceda1317d4d51e5a7d3
James Cary <eliteeggnog@gmail.com>
2024-04-13 10:13:34 -0500

    clean up Header cmp

---
+++ app/Header.tsx
@@ -0,0 +1,14 @@
+export default function Header() {
+  return (
+    <div className="grid justify-items-center animate-in">
+      <div className="sr-only">Who Should We Yell At?!</div>
+      <h1 className="text-5xl font-extrabold">
+        Who Should We Yell At?!
+      </h1>
+      <h2 className="text-md font-light">
+        Find out which elected official you can complain to about your policy
+        gripe
+      </h2>
+    </div>
+  );
+}
