d0af4c70c83922e0f5aafcd8f1c76e41eb8f3ec2
ianad <ianad@duck.com>
2024-04-13 13:53:39 -0400

    formatting

--- app/AutoListbox.tsx
+++ app/AutoListbox.tsx
@@ -2,29 +2,29 @@
 import React from "react";
 import { Autocomplete, AutocompleteItem } from "@nextui-org/react";
 
-export default function AutoListbox(
-  { data, indefiniteArticle, topic }: {
-    data: { name: string }[] | null;
-    indefiniteArticle: string;
-    topic: string;
-  },
-) {
-  return (
-    data
-      ? (
-        <>
-          <Autocomplete
-            defaultItems={data}
-            label={`Pick ${indefiniteArticle} ${topic}`}
-            placeholder={`Search for ${indefiniteArticle} ${topic}`}
-            className="max-w-xs"
-          >
-            {(datum) => (
-              <AutocompleteItem key={datum.name}>{datum.name}</AutocompleteItem>
-            )}
-          </Autocomplete>
-        </>
-      )
-      : <></>
+export default function AutoListbox({
+  data,
+  indefiniteArticle,
+  topic,
+}: {
+  data: { name: string }[] | null;
+  indefiniteArticle: string;
+  topic: string;
+}) {
+  return data ? (
+    <>
+      <Autocomplete
+        defaultItems={data}
+        label={`Pick ${indefiniteArticle} ${topic}`}
+        placeholder={`Search for ${indefiniteArticle} ${topic}`}
+        className="max-w-xs"
+      >
+        {(datum) => (
+          <AutocompleteItem key={datum.name}>{datum.name}</AutocompleteItem>
+        )}
+      </Autocomplete>
+    </>
+  ) : (
+    <></>
   );
 }
